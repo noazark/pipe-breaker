41e074e9198914244a3f3070da41e9039ed1d9c7
Noah H. Smith <noah@batteredoats.com>
2018-07-07 14:34:15 -0700

    moving utils out

--- src/Level.js
+++ src/Level.js
@@ -1,40 +1,5 @@
 import React, {Component} from 'react'
-import {findStartPos, findEndPos} from './utils'
-
-function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
-
-  return {
-    x: centerX + (radius * Math.cos(angleInRadians)),
-    y: centerY + (radius * Math.sin(angleInRadians))
-  };
-}
-
-function describeArc(innerRadius, outerRadius, startAngle, endAngle, padding){
-    const x = 0
-    const y= 0
-
-    const _paddingInner = 360 - 360 * (2 * Math.PI * innerRadius - padding) / (2 * Math.PI * innerRadius)
-    const _paddingOuter = 360 - 360 * (2 * Math.PI * outerRadius - padding) / (2 * Math.PI * outerRadius)
-
-    const start0 = polarToCartesian(x, y, innerRadius, startAngle + _paddingOuter/2);
-    const end0 = polarToCartesian(x, y, innerRadius, endAngle - _paddingOuter/2);
-    const end1 = polarToCartesian(x, y, outerRadius, endAngle - _paddingInner/2);
-    const start1 = polarToCartesian(x, y, outerRadius, startAngle + _paddingInner/2);
-
-    const largeArcFlag = endAngle - startAngle <= 180 ? "1" : "0";
-    const largeArcFlagInner = endAngle - startAngle <= 180 ? "0" : "1";
-
-    var d = [
-        "M", [start0.x, start0.y].join(','),
-        "A", [innerRadius, innerRadius, 0, 0, largeArcFlag, end0.x, end0.y].join(','),
-        "L", [end1.x, end1.y].join(','),
-        "A", [outerRadius, outerRadius, 0, 0, largeArcFlagInner, start1.x, start1.y].join(','),
-        "Z"
-    ].join("");
-
-    return d;
-}
+import {findStartPos, findEndPos, describeArc} from './utils'
 
 export default class Level extends Component {
   constructor (props) {
