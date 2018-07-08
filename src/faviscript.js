d4efd67ff56d42c6617d3870568b2258350f462b
Noah H. Smith <noah@batteredoats.com>
2018-07-07 20:08:29 -0700

    new favicon

--- src/faviscript.js
+++ src/faviscript.js
@@ -3,63 +3,64 @@ var centerX = this.width / 2;
 var centerY = this.height / 2;
 var pad = width >= 48 ? Math.round(this.width * 0.10) : 0;
 var radius = (this.width - pad * 2) / 2;
-var innerRadius = radius * (width >= 128 ? 0.9 : 1);
+var innerRadius = radius * .5;
+var padWidth = this.width * .05
 
+function randomLevel(pads=10, min=5, max=15) {
+  return (new Array(pads))
+    .fill(0)
+    .map(() => Math.ceil(Math.random() * max) + min)
+}
 
-this.fillStyle = '#fff';
+var levels = [
+  [3, 2, 3, 2],
+  [4, 4, 3, 3, 4],
+  [2, 4, 3, 3, 2],
+  [3, 2, 4, 2, 4],
+  [3, 2, 2, 4, 2]
+]
 
-this.rect(0, 0, this.width, this.height)
+console.log(levels);
+
+drawShell(this, centerX, centerY, innerRadius, 2);
+this.fillStyle = '#DB4A4A';
 this.fill();
 
-this.fillStyle = '#000';
-this.strokeStyle = '#fff';
+levels.forEach((level, l) => {
+    const ir = innerRadius + padWidth + (2 * padWidth * l)
+    level.forEach((pad, i) => {
+        drawPad(this, centerX, centerY, ir, padWidth, findStartPos(level, i), findEndPos(level, i));
+        this.fillStyle = '#333';
+        this.fill();
+    })
+})
 
-drawShell(this, centerX, centerY, this.width / 2, .2, 1.2);
-this.fill();
-this.stroke();
-
-drawShell(this, centerX, centerY, this.width / 2, .3, 1.8);
-this.fill();
-this.stroke();
-
-drawShell(this, centerX, centerY, this.width / 2, .3, 1.8);
-this.fill();
-this.stroke();
-
-drawShell(this, centerX, centerY, this.width / 2, 1, 3.7);
-this.fill();
-this.stroke();
-
-drawShell(this, centerX, centerY, this.width / 3, 2);
-this.fillStyle = '#FFF';
-this.fill();
-
-this.fillStyle = '#000';
-this.strokeStyle = '#fff';
-
-drawShell(this, centerX, centerY, this.width / 3 - .5, .3, 2);
-this.fill();
-this.stroke();
-
-drawShell(this, centerX, centerY, this.width / 3 - .5, 1.1, 2.9);
-this.fill();
-this.stroke();
-
-drawShell(this, centerX, centerY, this.width / 3 - .5, .5, 5);
-this.fill();
-this.stroke();
-
-drawShell(this, centerX, centerY, this.width / 6, 2);
-this.fillStyle = '#FFF';
-this.fill();
-
-drawShell(this, centerX, centerY, this.width / 6 - 1, 2);
-this.fillStyle = '#FF0000';
-this.fill();
-
-function drawShell(ctx, x, y, radius, scale, offset=0) {
+function drawShell(ctx, x, y, radius, scale) {
     ctx.beginPath();
-    ctx.arc(x, y, radius, offset, scale * Math.PI + offset, false);
-    ctx.lineTo(x, y);
+    ctx.arc(x, y, radius, 0, scale * Math.PI, false);
     ctx.closePath();
+}
+
+function drawPad(ctx, x, y, innerRadius, padWidth, start, end) {
+    const outerRadius = innerRadius + padWidth
+
+    const _paddingInner = 1 - (4 * Math.PI * innerRadius - padWidth) / (4 * Math.PI * innerRadius)
+    const _paddingOuter = 1 - (4 * Math.PI * outerRadius - padWidth) / (4 * Math.PI * outerRadius)
+
+    ctx.beginPath();
+    ctx.arc(x, y, innerRadius, (end - _paddingInner) * 2 * Math.PI, (start + _paddingOuter) * 2 * Math.PI, true);
+    ctx.arc(x, y, outerRadius, (start + _paddingOuter) * 2 * Math.PI, (end - _paddingInner) * 2* Math.PI, false);
+    ctx.closePath();
+}
+
+function sum(arr) {
+  return arr.reduce((agg, p) => agg + p, 0)
+}
+
+function findStartPos(level, i) {
+  return sum(level.slice(0, i)) / sum(level)
+}
+
+function findEndPos(level, i) {
+  return sum(level.slice(0, i + 1)) / sum(level)
 }
