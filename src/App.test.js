6ec591bd10f1699542c03612d95ac7c45d44f759
Noah H. Smith <noah@batteredoats.com>
2024-11-03 10:26:14 -0800

    fixing broken packages due to upgrade

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+import '@testing-library/jest-dom'
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders game board', () => {
   render(<App />);
-  const gameId = screen.getByID('game');
-  expect(gameId).toBeInTheDocument();
+  const playBtn = screen.getByText('play');
+  expect(playBtn).toBeInTheDocument();
 });
